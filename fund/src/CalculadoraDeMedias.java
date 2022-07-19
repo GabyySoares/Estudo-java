a703eb821ffe70a30fb84aeef8485d37ceff5bb3
Gabriela Soares <gabyyysoaarees@gmail.com>
2022-07-19 17:13:22 -0300

    main : Meu primeiro Commit

---
+++ fund/src/CalculadoraDeMedias.java
@@ -0,0 +1,11 @@
+public class CalculadoraDeMedias {
+
+    public static double lancarMedia(double nota1 , double nota2){
+
+        double media = (nota1 + nota2) /2;
+
+
+        return media;
+    }
+
+}
---
+++ fund/src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+        double resultado = CalculadoraDeMedias.lancarMedia(4.0,4.0);
+
+
+        String string = "gabriels";
+
+        System.out.println(resultado);
+    }
+
+}
